bcde06c4f2f35a0c11e7e8ba59b994523747d8d3
SuryaR001 <srosadha@gmail.com>
2025-06-03 13:07:19 +0700

    fix:hapus barang

--- app/admin/data_barang/hapus_barang.php
+++ app/admin/data_barang/hapus_barang.php
@@ -1,4 +1,7 @@
 <?php
+
+use Mpdf\Tag\Select;
+
 session_start();
 if (!isset($_SESSION["login"])) {
     header("Location: ../../auth/login.php?pesan=belum_login");
@@ -12,10 +15,14 @@ if (isset($_GET['id'])) {
     $idstock = intval($_GET['id']);
     $idbarang = intval($_GET['id_barang']);
     $id_toko = $_SESSION['id_toko'];
-
+    $select_stock = mysqli_query($conn, "SELECT * FROM stock");
+    $barang=mysqli_query($conn,"SELECT * FROM barang");
+    if(mysqli_fetch_assoc($select_stock)>0){
+        $hapus = mysqli_query($conn, "DELETE FROM stock WHERE id_stock = $idstock ");
+    }else{
+        $hapus_barang = mysqli_query($conn, "DELETE FROM barang WHERE id_barang = $idbarang");
+    }
     // Hapus stock barang dari toko ini saja
-    $hapus = mysqli_query($conn, "DELETE FROM stock WHERE id_stock = $idstock ");
-    $hapus_barang = mysqli_query($conn, "DELETE FROM barang WHERE id_barang = $idbarang");
     if ($hapus && mysqli_affected_rows($conn) > 0) {
         $_SESSION['berhasil'] = "Barang berhasil dihapus dari stok toko!";
     } else {
